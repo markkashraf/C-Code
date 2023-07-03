dd5dd2362c3af103aff2bde5233bed6039358055
markkashraf <mark2001rko2001@gmail.com>
2023-07-03 19:53:25 +0200

    Added Embedded C

---
+++ Unit 3 - Embedded C/Assignment 1/my_platform_types.h
@@ -0,0 +1,31 @@
+#ifndef MY_PLATFORM_TYPES_H_
+#define MY_PLATFORM_TYPES_H_
+
+
+/*8- bit integers*/
+typedef char sint8;
+typedef unsigned char uint8;
+typedef volatile char vint8;
+typedef volatile unsigned char vuint8;
+
+
+/*16 bit integers*/
+typedef short sint16;
+typedef unsigned short uint16;
+typedef short vint16;
+typedef unsigned short vuint16;
+
+/*32 bit integers*/
+typedef int sint32;
+typedef unsigned int uint32;
+typedef int vint32;
+typedef unsigned int vuint32;
+
+
+
+#endif
+
+
+
+
+
---
+++ Unit 3 - Embedded C/Assignment 1/platform_types.h
@@ -0,0 +1,31 @@
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+
+#include <stdint.h>
+
+/*8- bit integers*/
+typedef int8_t sint8;
+typedef uint8_t uint8;
+typedef volatile int8_t vint8;
+typedef volatile unsigned uint8_t vuint8;
+
+
+/*16 bit integers*/
+typedef int16_t sint16;
+typedef uint16_t uint16;
+typedef volatile int16_t vint16;
+typedef volatile uint16_t vuint16;
+
+/*32 bit integers*/
+typedef int32_t sint32;
+typedef uint32_t uint32;
+typedef volatile int32_t vint32;
+typedef volatile uint32_t vuint32;
+
+
+#endif
+
+
+
+
+
